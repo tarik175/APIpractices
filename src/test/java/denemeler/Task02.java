30009fcce8d615631f9029c0b3dab914d6070280
Tarık <ahmettarikzengin2002@gmail.com>
2022-12-15 23:27:08 +0300

    new test

--- src/test/java/denemeler/Task02.java
+++ src/test/java/denemeler/Task02.java
@@ -46,4 +46,17 @@ public class Task02 {
 
 
     }
+    @Test
+    public void test3(){
+        String url="https://restful-booker.herokuapp.com/booking/10 ";
+        Response response= RestAssured.get(url);
+        response.prettyPrint();
+        response.then().assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                statusLine("HTTP/1.1 200 OK").
+                header("Server","Cowboy");
+
+
+    }
 }
