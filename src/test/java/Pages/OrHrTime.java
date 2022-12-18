f9c030d7d4bef2b877ab51147db744350b323990
Tarık <ahmettarikzengin2002@gmail.com>
2022-12-18 23:51:27 +0300

    very small addition

--- src/test/java/Pages/OrHrTime.java
+++ src/test/java/Pages/OrHrTime.java
@@ -9,5 +9,8 @@ public class OrHrTime {
     @FindBy(xpath = "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div/form/div[3]/button")
     public WebElement punchInBtn;
 
+    @FindBy(xpath = "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div/form/div[3]/button")
+    public WebElement punchInBtn2;
+
 
 }
